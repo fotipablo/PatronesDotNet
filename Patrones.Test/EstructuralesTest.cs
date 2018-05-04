68dd3774947fd4be17d75d76ee61a09ac6b97df2
Pablo J. Foti <foti.pablo@gmail.com>
2018-05-04 17:32:10 -0300

    Agrego Adapter2Test()

--- Patrones.Test/EstructuralesTest.cs
+++ Patrones.Test/EstructuralesTest.cs
@@ -6,6 +6,7 @@ using Patrones.Creacionales.Prototype;
 using Patrones.Creacionales.Singleton;
 using Patrones.Creacionales.AbstractFactory;
 using Patrones.Estructural.Adapter;
+using Patrones.Estructural.Adapter2;
 
 namespace Patrones.Test
 {
@@ -28,6 +29,26 @@ namespace Patrones.Test
 
             Compound ethanol = new RichCompound("Ethanol");
             ethanol.Display();
+        }
+
+        [TestMethod]
+        public void Adapter2Test()
+        {
+            PersonaVieja vieja = new PersonaVieja();
+            vieja.nombre = "Mirtha";
+            vieja.apellido = "Legrand";
+            vieja.fechaNacimiento = new DateTime(1910, 1, 1);
+
+            ViejaToNuevaAdapter nueva = new ViejaToNuevaAdapter(vieja);
+            Console.WriteLine("Edad: " + nueva.edad);
+            Console.WriteLine("Nombre: " + nueva.nombre);
+
+            Console.WriteLine("Paso por el cirujano...");
+
+            nueva.edad = 30;
+            nueva.nombre = "China Suarez";
+            Console.WriteLine("Edad: " + nueva.edad);
+            Console.WriteLine("Nombre: " + nueva.nombre);
         }
     }
 }
