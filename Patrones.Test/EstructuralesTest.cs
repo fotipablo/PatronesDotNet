6188cc6cc40c0e9ccb0d468e099c59bcb8487b8e
Pablo J. Foti <foti.pablo@gmail.com>
2018-05-04 16:07:22 -0300

    Creo nueva clase de test con el método AdapterTest()

---
+++ Patrones.Test/EstructuralesTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patrones.Creacionales.BuilderTest;
+using Patrones.Creacionales.FactoryMethod;
+using Patrones.Creacionales.Prototype;
+using Patrones.Creacionales.Singleton;
+using Patrones.Creacionales.AbstractFactory;
+using Patrones.Estructural.Adapter;
+
+namespace Patrones.Test
+{
+    [TestClass]
+    public class EstructuralesTest
+    {
+        [TestMethod]
+        public void AdapterTest()
+        {
+            // Non-adapted chemical compound
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+        }
+    }
+}
