7de2a08173aebac953e34beb8571ed153497d44d
Pablo J. Foti <foti.pablo@gmail.com>
2018-05-04 15:49:24 -0300

    Agrego método SingletonTest()

--- Patrones.Test/CreacionalesTest.cs
+++ Patrones.Test/CreacionalesTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Patrones.Creacionales.BuilderTest;
 using Patrones.Creacionales.FactoryMethod;
+using Patrones.Creacionales.Prototype;
+using Patrones.Creacionales.Singleton;
 
 namespace Patrones.Test
 {
@@ -37,6 +39,50 @@ namespace Patrones.Test
             TrianguloFactory factory = new TrianguloFactory();
             Triangulo triangulo = factory.createTriangulo(10, 20, 30);
             Console.WriteLine(triangulo.getDescripcion());
+        }
+
+        [TestMethod]
+        public void PrototypeTest()
+        {
+            ColorManager colormanager = new ColorManager();
+
+            // Initialize with standard colors
+            colormanager["red"] = new Patrones.Creacionales.Prototype.Color(255, 0, 0);
+            colormanager["green"] = new Patrones.Creacionales.Prototype.Color(0, 255, 0);
+            colormanager["blue"] = new Patrones.Creacionales.Prototype.Color(0, 0, 255);
+
+            // User adds personalized colors
+            colormanager["angry"] = new Patrones.Creacionales.Prototype.Color(255, 54, 0);
+            colormanager["peace"] = new Patrones.Creacionales.Prototype.Color(128, 211, 128);
+            colormanager["flame"] = new Patrones.Creacionales.Prototype.Color(211, 34, 20);
+
+            // User clones selected colors
+            Patrones.Creacionales.Prototype.Color color1 = colormanager["red"].Clone() as Patrones.Creacionales.Prototype.Color;
+            Patrones.Creacionales.Prototype.Color color2 = colormanager["peace"].Clone() as Patrones.Creacionales.Prototype.Color;
+            Patrones.Creacionales.Prototype.Color color3 = colormanager["flame"].Clone() as Patrones.Creacionales.Prototype.Color;
+        }
+
+        [TestMethod]
+        public void SingletonTest()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
         }
     }
 }
